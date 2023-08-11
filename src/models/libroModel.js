27d0de2e70e1958e249e2d8f57013ba63a9bb6b9
leandromaydana <leandro-maydana@hotmail.com>
2023-08-11 01:24:35 -0300

    Tests Unitarios y de Integración

--- src/models/libroModel.js
+++ src/models/libroModel.js
@@ -1,6 +1,22 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect(process.env.MONGO_DB, {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+// });
+
+// const LibroSchema = new mongoose.Schema({
+//   titulo: String,
+//   autor: String
+// }, { collection: 'libros' });
+
+// const Libro = mongoose.model('Libro', LibroSchema);
+
+// module.exports = Libro;
+
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_DB, {
+mongoose.connect("mongodb://127.0.0.1:27017/biblioteca", {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
